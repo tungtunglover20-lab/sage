c0f1267a5b512416c5c7237d7b83846360ccdd29
tungtunglover20-lab <tungtunglover20@gmail.com>
2026-08-09 01:01:19 -0400

    Update lore.js

--- src/commands/lore.js
+++ src/commands/lore.js
@@ -9,7 +9,7 @@ import config from "../config/env.js";
 const GOOGLE_DOC_ID = "YOUR_GOOGLE_DOC_ID_HERE";
 
 const GOOGLE_DOC_URL =
-  `https://docs.google.com/document/d/${GOOGLE_DOC_ID}/export?format=txt`;
+  `https://docs.google.com/document/d/1FUUf-MI8z9McPNWk-8bqUTL96Q89lcxJ6l__yANXD0c/export?format=txt`;
 
 const ai = new OpenAI({
   apiKey: config.openRouter.apiKey,
