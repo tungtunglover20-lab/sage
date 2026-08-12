e77efd634ac25d063e90f6614f1caeaae895dbf0
tungtunglover20-lab <tungtunglover20@gmail.com>
2026-08-11 20:53:33 -0400

    Update stop.js

--- src/commands/stop.js
+++ src/commands/stop.js
@@ -1,18 +1,1 @@
-import { SlashCommandBuilder } from "discord.js";
 
-import musicManager from "../music/musicManager.js";
-
-export const data = new SlashCommandBuilder()
-    .setName("stop")
-    .setDescription("Stop music and leave the voice channel.");
-
-export async function execute(interaction) {
-    const music = musicManager.get(interaction.guild.id);
-
-    music.stop();
-    music.destroy();
-
-    await interaction.reply(
-        "⏹️ Stopped the music and left the voice channel."
-    );
-}
